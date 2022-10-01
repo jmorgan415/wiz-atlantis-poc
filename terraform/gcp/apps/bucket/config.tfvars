f4f10cf0207d2cb30ebe4abb4ca0dc6976519958
Jeff Morgan <jmorgan415@gmail.com>
2022-09-30 17:14:15 -0700

    added gcp tf config

---
+++ terraform/gcp/apps/bucket/config.tfvars
@@ -0,0 +1,7 @@
+config = {
+  "gcp-demo" = {
+    "bucket_name" = "wiz-demo-atlantis-gcp"
+    "project"     = "jeff-wiz-demo"
+    "region"      = "us-east5"
+  }
+}
---
+++ terraform/gcp/apps/bucket/main.tf
@@ -0,0 +1,13 @@
+# AWS Info
+provider "google" {
+  region      = var.config[terraform.workspace].region
+  project     = var.config[terraform.workspace].project
+  credentials = "/gcp.json"
+}
+
+# Create bucket
+resource "google_storage_bucket" "demo" {
+  name          = var.config[terraform.workspace].bucket_name
+  location      = "US"
+  force_destroy = true
+}
