fc1320b761232d48254b121be3486e1599a7a61a
Jeff Morgan <jmorgan415@gmail.com>
2022-10-10 23:42:31 -0700

    updated azure provider

--- terraform/azure/apps/rg/vars.tf
+++ terraform/azure/apps/rg/vars.tf
@@ -3,27 +3,3 @@ variable "rg_name" {
   type        = string
   default     = ""
 }
-
-variable "arm_client_id" {
-  description = "Azure client id"
-  type        = string
-  default     = null
-}
-
-variable "arm_client_secret" {
-  description = "Azure secret id"
-  type        = string
-  default     = null
-}
-
-variable "arm_subscription_id" {
-  description = "Azure subscription id"
-  type        = string
-  default     = null
-}
-
-variable "arm_tenant_id" {
-  description = "Azure tenant id"
-  type        = string
-  default     = null
-}
